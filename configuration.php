1f08705e4a9b07bfa62e9a15a65324c90f257ec4
Maulvi Inayat Ali <maulvi67@gmail.com>
2017-10-13 09:42:22 +0700

    konfigurasi global

---
+++ configuration.php
@@ -0,0 +1,12 @@
+<?php 
+
+	class MConfig{
+		var $_site_url = 'http://localhost/tugas-sbd';
+		var $_document_root = 'C:/xampp/htdocs/tugas-sbd';
+		var $_host_name = 'localhost';
+		var $_database_user = 'root';
+		var $_database_name = 'db_tugas_sbd';
+		var $_database_password = '';
+		var $_table_prefix = 'tbl_';
+	}
+ ?>
