427a307dfa168f3708e5fc7f3b1d70b7f7319c29
Daniyal Ahmad Rizaldhi <onieln14@gmail.com>
2018-02-07 11:48:43 +0700

    update - fix conflict

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 defined('BASEPATH') or exit('No direct script access allowed');
 date_default_timezone_set("Asia/Jakarta");
 
@@ -135,7 +134,7 @@ class Home extends CI_Controller
         $excel->getActiveSheet()->getStyle('E3')->applyFromArray($style_col);
         // Panggil function view yang ada di SiswaModel untuk menampilkan semua data siswanya
         $transaksi = $this->M_dashboard->laporan_transaksi();
-        $tanggal   = $this->M_dashboard->tanggal_transaksi(); 
+        $tanggal   = $this->M_dashboard->tanggal_transaksi();
         $no = 1; // Untuk penomoran tabel, di awal set dengan 1
         $numrow = 4; // Set baris pertama untuk isi tabel adalah baris ke 4
         foreach ($transaksi as $data) { // Lakukan looping pada variabel siswa
@@ -240,207 +239,6 @@ class Home extends CI_Controller
     }
 
     protected function drop_foreign_key($table, $constraint)
-=======
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-class Home extends CI_Controller {
-	public function __construct()
-	{
-		parent::__construct();
-		$this->load->model('M_security');
-		$this->load->model('manajer/M_dashboard');
-	}
-
-	public function index()
-	{
-		$this->M_security->cek_security();
-		$level = $this->session->userdata('level');
-		$key   = $this->session->userdata('id_pegawai');
-
-		$data['judul']		= 'Home';
-		$data['sub_judul']	= 'Dashboard';
-		$data['base_link'] 	= '';
-
-
-
-		if($level == 'manajer')
-		{
-			$data['jml_meja']		= $this->M_dashboard->meja_count();
-			$data['jml_pemesanan']  = $this->M_dashboard->pemesanan_count();
-			$data['jml_pegawai']	= $this->M_dashboard->pegawai_count();
-			$data['jml_makanan']	= $this->M_dashboard->makanan_count();
-			$data['jml_database']	= $this->M_dashboard->db_count();
-			$data['data_transaksi'] = $this->M_dashboard->laporan_transaksi();
-			$data['tabel'] 			= $this->M_dashboard->tampiltabel(); 
-
-			$data['user'] 			= 'MANAJER';
-			$data['content'] 		= 'manajer/v_dashboard';
-
-		}
-		else if($level == 'kasir')
-		{
-			$data['jml_pemesanan']  = $this->M_dashboard->pemesanan_count();
-
-			$data['user'] 			= 'KASIR';
-			$data['content'] 		= 'kasir/v_dashboard';
-		}
-
-		$this->load->view('v_home',$data);
-	}
-
-	public function tanggal()
-	{
-		$this->M_security->cek_security();
-		$data['judul']		= 'Home';
-		$data['sub_judul']	= 'Dashboard';
-		$data['base_link'] 	= '';
-
-		$data['jml_meja']		= $this->M_dashboard->meja_count();
-		$data['jml_pemesanan']  = $this->M_dashboard->pemesanan_count();
-		$data['jml_pegawai']	= $this->M_dashboard->pegawai_count();
-		$data['jml_makanan']	= $this->M_dashboard->makanan_count();
-		$data['jml_database']	= $this->M_dashboard->db_count();
-		$data['data_transaksi'] = $this->M_dashboard->laporan_transaksi();
-		$data['tabel'] 			= $this->M_dashboard->tampiltabel(); 
-
-		$data['user'] 			= 'MANAJER';
-		$data['content'] 		= 'manajer/v_dashboard';
-
-		
-		$tgl_awal  = $this->input->post('tanggal_awal');
-		$tgl_akhir = $this->input->post('tanggal_akhir');
-
-		$data['tgl_awal']  = $tgl_awal;
-		$data['tgl_akhir'] = $tgl_akhir;
-
-		$data['tanggal_transaksi'] = $this->M_dashboard->tanggal_transaksi($tgl_awal,$tgl_akhir);
-
-		$this->load->view('v_home',$data);
-
-		
-	}
-	public function export(){
-		// Load plugin PHPExcel nya
-		include APPPATH.'third_party/PHPExcel/PHPExcel.php';
-
-		// Panggil class PHPExcel nya
-		$excel = new PHPExcel();
-		// Settingan awal fil excel
-		$excel->getProperties()->setCreator('MDI')
-					 ->setLastModifiedBy('MDI')
-					 ->setTitle("Laporan Transaksi")
-					 ->setSubject("Transaksi")
-					 ->setDescription("Laporan Semua Data Transaksi")
-					 ->setKeywords("Laporan Transaksi");
-		// Buat sebuah variabel untuk menampung pengaturan style dari header tabel
-		$style_col = array(
-		  'font' => array('bold' => true), // Set font nya jadi bold
-		  'alignment' => array(
-			'horizontal' => PHPExcel_Style_Alignment::HORIZONTAL_CENTER, // Set text jadi ditengah secara horizontal (center)
-			'vertical' => PHPExcel_Style_Alignment::VERTICAL_CENTER // Set text jadi di tengah secara vertical (middle)
-		  ),
-		  'borders' => array(
-			'top' => array('style'  => PHPExcel_Style_Border::BORDER_THIN), // Set border top dengan garis tipis
-			'right' => array('style'  => PHPExcel_Style_Border::BORDER_THIN),  // Set border right dengan garis tipis
-			'bottom' => array('style'  => PHPExcel_Style_Border::BORDER_THIN), // Set border bottom dengan garis tipis
-			'left' => array('style'  => PHPExcel_Style_Border::BORDER_THIN) // Set border left dengan garis tipis
-		  )
-		);
-		// Buat sebuah variabel untuk menampung pengaturan style dari isi tabel
-		$style_row = array(
-		  'alignment' => array(
-			'vertical' => PHPExcel_Style_Alignment::VERTICAL_CENTER // Set text jadi di tengah secara vertical (middle)
-		  ),
-		  'borders' => array(
-			'top' => array('style'  => PHPExcel_Style_Border::BORDER_THIN), // Set border top dengan garis tipis
-			'right' => array('style'  => PHPExcel_Style_Border::BORDER_THIN),  // Set border right dengan garis tipis
-			'bottom' => array('style'  => PHPExcel_Style_Border::BORDER_THIN), // Set border bottom dengan garis tipis
-			'left' => array('style'  => PHPExcel_Style_Border::BORDER_THIN) // Set border left dengan garis tipis
-		  )
-		);
-		$excel->setActiveSheetIndex(0)->setCellValue('A1', "LAPORAN TRANSAKSI"); // Set kolom A1 dengan tulisan "DATA SISWA"
-		$excel->getActiveSheet()->mergeCells('A1:E1'); // Set Merge Cell pada kolom A1 sampai E1
-		$excel->getActiveSheet()->getStyle('A1')->getFont()->setBold(TRUE); // Set bold kolom A1
-		$excel->getActiveSheet()->getStyle('A1')->getFont()->setSize(15); // Set font size 15 untuk kolom A1
-		$excel->getActiveSheet()->getStyle('A1')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER); // Set text center untuk kolom A1
-		// Buat header tabel nya pada baris ke 3
-		$excel->setActiveSheetIndex(0)->setCellValue('A3', "NO"); // Set kolom A3 dengan tulisan "NO"
-		$excel->setActiveSheetIndex(0)->setCellValue('B3', "ID TRANSAKSI"); // Set kolom B3 dengan tulisan "NIS"
-		$excel->setActiveSheetIndex(0)->setCellValue('C3', "NAMA PEMESAN"); // Set kolom C3 dengan tulisan "NAMA"
-		$excel->setActiveSheetIndex(0)->setCellValue('D3', "TANGGAL TRANSAKSI"); // Set kolom D3 dengan tulisan "JENIS KELAMIN"
-		$excel->setActiveSheetIndex(0)->setCellValue('E3', "NAMA PEGAWAI"); // Set kolom E3 dengan tulisan "ALAMAT"
-		// Apply style header yang telah kita buat tadi ke masing-masing kolom header
-		$excel->getActiveSheet()->getStyle('A3')->applyFromArray($style_col);
-		$excel->getActiveSheet()->getStyle('B3')->applyFromArray($style_col);
-		$excel->getActiveSheet()->getStyle('C3')->applyFromArray($style_col);
-		$excel->getActiveSheet()->getStyle('D3')->applyFromArray($style_col);
-		$excel->getActiveSheet()->getStyle('E3')->applyFromArray($style_col);
-		// Panggil function view yang ada di SiswaModel untuk menampilkan semua data siswanya
-		$transaksi = $this->M_dashboard->laporan_transaksi();
-		$tanggal   = $this->M_dashboard->tanggal_transaksi();
-		$no = 1; // Untuk penomoran tabel, di awal set dengan 1
-		$numrow = 4; // Set baris pertama untuk isi tabel adalah baris ke 4
-		foreach($transaksi as $data){ // Lakukan looping pada variabel siswa
-		  $excel->setActiveSheetIndex(0)->setCellValue('A'.$numrow, $no);
-		  $excel->setActiveSheetIndex(0)->setCellValue('B'.$numrow, $data->id_transaksi);
-		  $excel->setActiveSheetIndex(0)->setCellValue('C'.$numrow, $data->nama_pemesan);
-		  $excel->setActiveSheetIndex(0)->setCellValue('D'.$numrow, $data->tanggal);
-		  $excel->setActiveSheetIndex(0)->setCellValue('E'.$numrow, $data->nama_pegawai);
-
-		  // Apply style row yang telah kita buat tadi ke masing-masing baris (isi tabel)
-		  $excel->getActiveSheet()->getStyle('A'.$numrow)->applyFromArray($style_row);
-		  $excel->getActiveSheet()->getStyle('B'.$numrow)->applyFromArray($style_row);
-		  $excel->getActiveSheet()->getStyle('C'.$numrow)->applyFromArray($style_row);
-		  $excel->getActiveSheet()->getStyle('D'.$numrow)->applyFromArray($style_row);
-		  $excel->getActiveSheet()->getStyle('E'.$numrow)->applyFromArray($style_row);
-
-		  $no++; // Tambah 1 setiap kali looping
-		  $numrow++; // Tambah 1 setiap kali looping
-		}
-		// Set width kolom
-		$excel->getActiveSheet()->getColumnDimension('A')->setWidth(5); // Set width kolom A
-		$excel->getActiveSheet()->getColumnDimension('B')->setWidth(15); // Set width kolom B
-		$excel->getActiveSheet()->getColumnDimension('C')->setWidth(25); // Set width kolom C
-		$excel->getActiveSheet()->getColumnDimension('D')->setWidth(20); // Set width kolom D
-		$excel->getActiveSheet()->getColumnDimension('E')->setWidth(30); // Set width kolom E
-
-		// Set height semua kolom menjadi auto (mengikuti height isi dari kolommnya, jadi otomatis)
-		$excel->getActiveSheet()->getDefaultRowDimension()->setRowHeight(-1);
-		// Set orientasi kertas jadi LANDSCAPE
-		$excel->getActiveSheet()->getPageSetup()->setOrientation(PHPExcel_Worksheet_PageSetup::ORIENTATION_LANDSCAPE);
-		// Set judul file excel nya
-		$excel->getActiveSheet(0)->setTitle("Laporan Data Transaksi");
-		$excel->setActiveSheetIndex(0);
-		// Proses file excel
-		header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-		header('Content-Disposition: attachment; filename="Data Transaksi.xlsx"'); // Set nama file excel nya
-		header('Cache-Control: max-age=0');
-		$write = PHPExcel_IOFactory::createWriter($excel, 'Excel2007');
-		$write->save('php://output');
-	  }
-
-	  public function backupalldb()
-	  {
-		  $this->load->dbutil();
-		  $this->load->helper('file');
-		  $this->load->helper('download');
-
-		  $aturan = array(
-			  'format'		=>'zip',
-			  'filename'	=>'db_backup.sql'
-		  );
-
-		  $backup = $this->dbutil->backup($aturan);
-		  $nama_database = 'backup-on-'.date('Y-m-d-H-i-s').'.zip';
-
-		  $simpan = 'backup/'.$nama_database;
- 	  
-		  write_file($simpan, $backup);	 
-		  force_download($nama_database,$backup);
-	  }
-
-	  public function backupdb()
->>>>>>> dashboard
     {
         return "ALTER TABLE {$table} DROP FOREIGN KEY {$constraint}";
     }
@@ -506,3 +304,4 @@ class Home extends CI_Controller {
 
 /* End of file Home.php */
 /* Location: ./application/controllers/Home.php */
+	
